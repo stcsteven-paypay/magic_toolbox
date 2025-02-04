f059ecf98e0dad73d6ac80850595efb5fc57d4f7
Steven Candra <steven.candra@paypay-corp.co.jp>
2025-02-04 10:40:27 +0900

    add conditional

--- yaml_replacer/YamlTool.py
+++ yaml_replacer/YamlTool.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+
 def replace(filter_file_path, replace_file_path):
   parent_dir = os.path.dirname(os.getcwd())
   with open(filter_file_path, "r") as fin:
@@ -12,13 +13,13 @@ def replace(filter_file_path, replace_file_path):
       if filter_lines != replace_lines:
         print("Filter and Replace files do not have the same number of lines")
         return
-      # print(filters)
-      # print(replaces)
       for filter, replace in zip(filters, replaces):
         for root, dir, files in os.walk(parent_dir):
-          if(root == os.path.join(parent_dir, "yaml_replacer")):
+          if (root == os.path.join(parent_dir, "yaml_replacer")):
             continue
           for file in files:
+            if not file.endswith(".yaml"):
+              continue
             with open(os.path.join(root, file), "r") as f:
               file_data = f.read()
             file_data = file_data.replace(filter, replace)
